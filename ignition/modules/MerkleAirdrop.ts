04b5da3832c227fee170ede937d7bddd1c91fe08
rhammy <olalekanrahman06@gmail.com>
2024-10-04 03:27:12 +0100

    deploy merkle aridrop

--- ignition/modules/MerkleAirdrop.ts
+++ ignition/modules/MerkleAirdrop.ts
@@ -1,12 +1,8 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-const { ethers } = require("ethers");
 
 const tokenAddress = "0x860fE8cF02690ED3a0385BEb3eF7f215281f9628";
-const merkleRoot = ethers.utils.hexlify(
-  ethers.utils.arrayify(
-    "0x389f5fc3d79eacc4e22b55e5b1cf0aa60f198b17aecdd4b9045090f5c37f9e7d"
-  )
-);
+const merkleRoot =
+  "0x389f5fc3d79eacc4e22b55e5b1cf0aa60f198b17aecdd4b9045090f5c37f9e7d";
 
 const MerkleAirdropModule = buildModule("MerkleAirdropModule", (m) => {
   const airdrop = m.contract("MerkleAirdrop", [tokenAddress, merkleRoot]);
